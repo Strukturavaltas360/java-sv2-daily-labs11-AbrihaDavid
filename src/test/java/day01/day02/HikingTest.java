424411319c211d8f028fdddcd4d85e474fb71d0d
AbrihaDavid <abrihadavid@gmail.com>
2022-01-04 15:44:04 +0100

    Daily task: done

---
+++ src/test/java/day01/day02/HikingTest.java
@@ -0,0 +1,27 @@
+package day01.day02;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HikingTest {
+
+    Hiking hiking = new Hiking();
+
+    @Test
+    void testWithNoElevationGain(){
+        List<Double> elevationList = Arrays.asList(50.0,40.0,30.0);
+        assertEquals(0,hiking.getPlusElevation(elevationList));
+    }
+
+    @Test
+    void testWithElevationGain(){
+        List<Double> elevationList = Arrays.asList(50.0,70.0,30.0,100.0);
+        assertEquals(90,hiking.getPlusElevation(elevationList));
+    }
+
+}
